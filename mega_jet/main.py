d02bd0c375a6fbf9dc60742425b81c094c70ea9b
Zack Connaughton <zaconnaughton@gmail.com>
2019-11-30 23:09:52 -0800

    Added sound effect library function and folder for sound effects. Space fires sound effect for laser shot. Generalized screen width and height to future usage. Moved get_pressed function outside of event loop in main loop so that continuous movement would be possible.

--- mega_jet/main.py
+++ mega_jet/main.py
@@ -12,15 +12,29 @@ def get_image(path):
         IMAGE_LIBRARY[path] = image
     return image
 
+SOUND_LIBRARY = {}
+def play_sound (path):
+    global SOUND_LIBRARY
+    sound = SOUND_LIBRARY.get(path)
+    if sound == None:
+        extended_path = path
+        print(extended_path)
+        sound = pygame.mixer.Sound(extended_path)
+        SOUND_LIBRARY[path] = sound
+    sound.play()
+    return None
+
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 done = False
 clock = pygame.time.Clock()
 
-x = 20
-y = 20
-speed = 3
+x = (SCREEN_WIDTH - 40) / 2
+y = (SCREEN_HEIGHT - 40)
+speed = 5
 
 pygame.mixer.music.load('music/basic_song_01.mp3')
 pygame.mixer.music.play(0)
@@ -37,15 +51,18 @@ while not done:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             done = True
 
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            y -= speed
-        if pressed[pygame.K_DOWN]:
-            y += speed
-        if pressed[pygame.K_LEFT]:
-            x -= speed
-        if pressed[pygame.K_RIGHT]:
-            x += speed
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            play_sound("sound_effects/laser_shot_01.wav")
+
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= speed
+    if pressed[pygame.K_DOWN]:
+        y += speed
+    if pressed[pygame.K_LEFT]:
+        x -= speed
+    if pressed[pygame.K_RIGHT]:
+        x += speed
 
     screen.fill((255, 255, 255))
     screen.blit(get_image('ball.png'), (x, y))
